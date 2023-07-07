798b1826eb16918bf3576360e9a9f6f5dd2c12c7
Gustavo Malimpensa <gumalimpensa@gmail.com>
2023-07-07 12:43:29 -0300

    Updating the data as requested

--- email/tratativas/empresa_email.py
+++ email/tratativas/empresa_email.py
@@ -7,8 +7,8 @@ from urllib.parse import quote_plus
 # Caminho completo para o arquivo principal (tratativa.py)
 caminho_principal = os.path.abspath(__file__)
 
-# Caminho completo para o arquivo resultados.json
-caminho_resultados_json = os.path.join(os.path.dirname(caminho_principal), '..', 'resultados', 'resultados.json')
+# Caminho completo para o arquivo telefone.json
+caminho_telefone_json = os.path.join(os.path.dirname(caminho_principal), '..', 'telefone', 'telefone.json')
 
 # Caminho completo para o arquivo empresas.json
 caminho_empresas_json = os.path.join(os.path.dirname(caminho_principal), '..', 'empresas.json')
@@ -17,9 +17,9 @@ caminho_empresas_json = os.path.join(os.path.dirname(caminho_principal), '..', '
 with open(caminho_empresas_json, 'r', encoding='utf-8') as arquivo_json:
     dados_empresas = json.load(arquivo_json)
 
-# Função para encontrar o email de uma empresa
-def encontrar_email_empresa(razao_social):
-    query = quote_plus(razao_social) + "+email"
+# Função para encontrar o telefone de uma empresa
+def encontrar_telefone_empresa(razao_social):
+    query = quote_plus(razao_social) + "+telefone"
 
     link = f"https://www.google.com/search?q={query}"
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
@@ -27,36 +27,36 @@ def encontrar_email_empresa(razao_social):
     requisicao = requests.get(link, headers=headers)  # requisição GET para obter o conteúdo da página
 
     if requisicao.status_code == 200:
-        email = BeautifulSoup(requisicao.text, "html.parser")  # criar um objeto BeautifulSoup para fazer o parsing do conteúdo HTML
+        telefone = BeautifulSoup(requisicao.text, "html.parser")  # criar um objeto BeautifulSoup para fazer o parsing do conteúdo HTML
 
-        email_empresa = email.find("cite", class_="apx8Vc tjvcx GvPZzd cHaqb")  # encontrar o elemento <cite> na página
+        telefone_empresa = telefone.find("span", class_="mw31Ze")  # encontrar o elemento <cite> na página
 
-        if email_empresa:
-            return email_empresa.get_text()
+        if telefone_empresa:
+            return telefone_empresa.get_text()
         else:
-            return "email não encontrado"
+            return "telefone não encontrado"
 
     return "Erro na requisição"
 
-# Lista para armazenar os resultados das empresas
-resultados_empresas = []
+# Lista para armazenar os telefone das empresas
+telefone_empresas = []
 
-# Iterar pelas empresas e encontrar o email de cada uma
+# Iterar pelas empresas e encontrar o telefone de cada uma
 for empresa in dados_empresas:
     razao_social = empresa["razao_social"]
-    email = encontrar_email_empresa(razao_social)
+    telefone = encontrar_telefone_empresa(razao_social)
     resultado_empresa = {
         "razao_social": razao_social,
-        "email": email
+        "telefone": telefone
     }
-    resultados_empresas.append(resultado_empresa)
+    telefone_empresas.append(resultado_empresa)
 
-# Criar a pasta de resultados, se não existir
-pasta_resultados = os.path.join(os.path.dirname(caminho_principal), '..', 'resultados')
-os.makedirs(pasta_resultados, exist_ok=True)
+# Criar a pasta de telefone, se não existir
+pasta_telefone = os.path.join(os.path.dirname(caminho_principal), '..', 'telefone')
+os.makedirs(pasta_telefone, exist_ok=True)
 
-# Salvar os resultados em um arquivo JSON (resultados.json)
-with open(caminho_resultados_json, 'w', encoding='utf-8') as arquivo_json:
-    json.dump(resultados_empresas, arquivo_json, indent=4)
+# Salvar os telefone em um arquivo JSON (telefone.json)
+with open(caminho_telefone_json, 'w', encoding='utf-8') as arquivo_json:
+    json.dump(telefone_empresas, arquivo_json, indent=4)
 
-print(f"Arquivo '{caminho_resultados_json}' criado com sucesso.")
+print(f"Arquivo '{caminho_telefone_json}' criado com sucesso.")
